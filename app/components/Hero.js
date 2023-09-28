9b944ad367dc1427dd5137500b32ca05c7336303
Kimi Nel <kimi.monique.nel@gmail.com>
2023-09-28 18:53:41 +0200

    feat: Add Hero Image

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,12 +1,13 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="w-full h-screen bg-green text-white text-[16rem] font-anton text-center">
+    <div className="relative w-full h-screen bg-green text-white text-[16rem] font-anton text-center">
       <motion.h1
-        className="uppercase"
+        className="uppercase relative z-10"
         initial={{ opacity: 0, scale: 0.5 }}
         whileInView={{ ease: "easeIn", opacity: 1, scale: 1 }}
         viewport={{ once: true }}
@@ -21,14 +22,38 @@ const Hero = () => {
       >
         Meat-free
       </motion.h1>
-      <motion.h1
-        className="uppercase"
+
+      <motion.div
+        className="absolute top-[26%] left-[40%] z-20"
         initial={{ opacity: 0, scale: 0.5 }}
         whileInView={{ ease: "easeIn", opacity: 1, scale: 1 }}
         viewport={{ once: true }}
         transition={{
           duration: 0.2,
           delay: 0.2,
+          scale: {
+            type: "spring",
+            damping: 10,
+            stiffness: 180,
+          },
+        }}
+      >
+        <Image
+          src="/Afro-Fusion-Spice-Bowl.png"
+          width={400}
+          height={400}
+          alt="Afro Fusion Spice Bowl"
+        />
+      </motion.div>
+
+      <motion.h1
+        className="uppercase relative z-30"
+        initial={{ opacity: 0, scale: 0.5 }}
+        whileInView={{ ease: "easeIn", opacity: 1, scale: 1 }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 0.2,
+          delay: 0.4,
           scale: {
             type: "spring",
             damping: 10,
