f216dba89ed635bc3a3f10c6ee4bdcebcf64697c
Kimi Nel <kimi.monique.nel@gmail.com>
2023-09-28 16:05:44 +0200

    feat: adding logo svg along with refactor

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,27 +1,32 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import * as logo from "../icons/logo.svg";
 
 const Header = () => {
   return (
-    <nav className="w-full h-28 px-28 flex justify-between items-center bg-green">
-      <ul
-        id="menu-desktop-left"
-        className="flex space-x-28 uppercase font-semibold text-lg text-white"
-      >
+    <nav className="w-full h-32 bg-green flex">
+      <ul className="w-full flex justify-around items-center uppercase font-semibold text-md text-white">
         <li>
           <a>About</a>
         </li>
         <li>
           <a>Products</a>
         </li>
-      </ul>
 
-      <a className="flex flex-col text-center text-white text-4xl font-extrabold">
-        <p>Eat</p>
-        <p>Curious</p>
-      </a>
+        <li>
+          <a>
+            <Image
+              src={logo}
+              width={175}
+              height={50}
+              alt="Eat Curious"
+              priority
+            />
+          </a>
+        </li>
 
-      <ul id="menu-desktop-right" className="flex space-x-28 uppercase">
-        <li className="text-lg text-white font-semibold">
+        <li>
           <a>Recipes</a>
         </li>
         <li className="bg-pink p-2 rounded-md text-green font-extrabold">
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
+import Header from "@components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "@components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <Header/>
         {children}
       </body>
     </html>
