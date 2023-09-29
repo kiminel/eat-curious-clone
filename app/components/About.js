5019828d39970cc11da5f67be51dc20f978a8806
Kimi Nel <kimi.monique.nel@gmail.com>
2023-09-29 17:40:08 +0200

    feat: create About page

--- app/components/About.js
+++ app/components/About.js
@@ -2,14 +2,39 @@
 import React from "react";
 import Image from "next/image";
 import * as wavy from "../icons/wavy.svg";
+import * as aboutSquiggle from "../icons/about-squiggle.svg";
+import * as pizzaMan from "../icons/pizza-man.svg";
 
 const About = () => {
   return (
     <div>
       <Image src={wavy} alt="wavy" className="w-full bg-green" />
-      <div className="text-white bg-pink">
-        <h1 className="text-4xl font-bold mb-4">About Us</h1>
-        <p>This is the about page content...</p>
+      <div className="absolute left-1/2 mt-16">
+        <Image
+          src={aboutSquiggle}
+          width={60}
+          height={60}
+          alt="About Squiggle"
+        />
+      </div>
+
+      <div className="min-h-screen w-full text-white bg-pink px-36 flex items-center">
+        <div className="grid-cols-2 w-full flex justify-evenly">
+          <div className="col-span-1 text-green space-y-6">
+            <h1 className=" text-8xl uppercase font-lilita">
+              Curious... By name, by nature
+            </h1>
+
+            <p className="pr-96">
+              It&apos;s what inspires us to whip up, throw together, tear,
+              shake, and break the rules – on a mission to redefine food for a
+              new generation.
+            </p>
+          </div>
+          <div className="col-span-1">
+            <Image src={pizzaMan} alt="pizza man" width={900} height={900} />
+          </div>
+        </div>
       </div>
     </div>
   );
