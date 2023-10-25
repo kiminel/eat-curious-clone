a47213a5d614e99ccbbc2d7cd67c07de7f753445
Kimi Nel <kimi.monique.nel@gmail.com>
2023-10-25 19:51:23 +0200

    refactor: page code refactor and cleanup

--- app/components/Products.js
+++ app/components/Products.js
@@ -1,87 +1,60 @@
 import React from "react";
 import Image from "next/image";
 
+const products = [
+  {
+    name: "Curious Pieces",
+    imageSrc: "/curiousPieces.png",
+    bg: "bg-lightGreen",
+  },
+  {
+    name: "Curious Mince",
+    imageSrc: "/curiousMince.png",
+    bg: "bg-mustardYellow",
+  },
+  {
+    name: "Curious Negg",
+    imageSrc: "/curiousNegg.png",
+    bg: "bg-lightGreen",
+  },
+  {
+    name: "Curious Tenders",
+    imageSrc: "/curiousTenders.png",
+    bg: "bg-lightGreen",
+  },
+  {
+    name: "Curious Bangers",
+    imageSrc: "/curiousBangers.png",
+    bg: "bg-red",
+  },
+  {
+    name: "Curious Fillets",
+    imageSrc: "/curiousFillets.png",
+    bg: "bg-lightGreen",
+  },
+];
+
 const Products = () => {
   return (
     <div className="w-full bg-green min-h-screen px-24 py-12">
       <div className="grid grid-cols-3 pt-4 gap-4 text-center">
-        <div className="bg-lightGreen p-4 rounded-md uppercase overflow-hidden relative h-[500px]">
-          <h1 className="text-[7rem] leading-none text-green font-anton py-2">
-            Curious Pieces
-          </h1>
-          <Image
-            className="absolute"
-            src="/curiousPieces.png"
-            width={400}
-            height={400}
-            alt="Curious Pieces"
-          />
-        </div>
-
-        <div className="bg-mustardYellow p-4 rounded-md uppercase overflow-hidden relative h-[500px]">
-          <h1 className="text-[7rem] leading-none text-green font-anton py-2">
-            Curious Mince
-          </h1>
-          <Image
-            className="absolute"
-            src="/curiousMince.png"
-            width={400}
-            height={400}
-            alt="Curious Mince"
-          />
-        </div>
-
-        <div className="bg-lightGreen  p-4 rounded-md uppercase overflow-hidden relative h-[500px]">
-          <h1 className="text-[7rem] leading-none text-green font-anton py-2">
-            Curious Negg
-          </h1>
-          <Image
-            className="absolute"
-            src="/curiousNegg.png"
-            width={400}
-            height={400}
-            alt="Curious Negg"
-          />
-        </div>
-
-        <div className="bg-lightGreen p-4  rounded-md uppercase overflow-hidden relative h-[500px]">
-          <h1 className="text-[7rem] leading-none text-green font-anton py-2">
-            Curious Tenders
-          </h1>
-          <Image
-            className="absolute"
-            src="/curiousTenders.png"
-            width={400}
-            height={400}
-            alt="Curious Tenders"
-          />
-        </div>
-
-        <div className="bg-red p-4  rounded-md uppercase overflow-hidden relative h-[500px]">
-          <h1 className="text-[7rem] leading-none text-green font-anton py-2">
-            Curious Bangers
-          </h1>
-          <Image
-            className="absolute"
-            src="/curiousBangers.png"
-            width={400}
-            height={400}
-            alt="Curious Bangers"
-          />
-        </div>
-
-        <div className="bg-lightGreen p-4  rounded-md uppercase overflow-hidden relative h-[500px]">
-          <h1 className="text-[7rem] leading-none text-green font-anton py-2">
-            Curious Fillets
-          </h1>
-          <Image
-            className="absolute"
-            src="/curiousFillets.png"
-            width={400}
-            height={400}
-            alt="Curious Fillets"
-          />
-        </div>
+        {products.map((product, index) => (
+          <div
+            key={index}
+            className={`${product.bg} p-4 rounded-md uppercase overflow-hidden relative h-[500px]`}
+          >
+            <h1 className="text-[7rem] leading-none text-green font-anton py-2">
+              {product.name}
+            </h1>
+            <Image
+              className="absolute"
+              src={product.imageSrc}
+              width={400}
+              height={400}
+              alt={product.name}
+            />
+          </div>
+        ))}
       </div>
       <div className="flex justify-center">
         <button className="bg-pink text-green font-anton uppercase mt-12 text-2xl py-2 px-4 rounded-sm">
